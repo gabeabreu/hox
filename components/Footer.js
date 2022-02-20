0606673cf78e7ee1aa899232617fdf99ecde21ae
Gabriel Abreu <84470114+gabeabreu@users.noreply.github.com>
2022-02-20 19:26:43 -0300

    responsive footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,22 @@
 export default function Footer() {
   return (
-    <div className="bg-[#f2f2f2] mb-5 w-full items-center text-center flex justify-around">
+    <div className="mb-5 border-t border-[#c0c0c0] w-full items-center text-center flex justify-around">
       <div>
-        <h2 className="text-2xl font-light">Contact me</h2>
-        <p>Email: gabriel010101@gmail.com</p>
-        <p>+55(61)99222-4543</p>
+        <h2 className="text-sm md:text-xl font-light">Contact me</h2>
+        <p className="text-sm md:text-sm">Email: gabriel010101@gmail.com</p>
+        <p className="text-sm md:text-sm">+55(61)99222-4543</p>
       </div>
       <div>
-        <h2 className="text-2xl font-light">Made by: Gabriel Abreu</h2>
-        <p>This isn&apos;t a real page</p>
-        <p>first time using next.js</p>
+        <h2 className="text-sm md:text-xl font-light">
+          Made by: Gabriel Abreu
+        </h2>
+        <p className="text-sm md:text-sm">This isn&apos;t a real page</p>
+        <p className="text-sm md:text-sm">first time using next.js</p>
       </div>
       <div>
-        <h2 className="text-2xl font-light">Computer Science</h2>
-        <p>web developer</p>
-        <p>React.js</p>
+        <h2 className="text-sm md:text-xl font-light">Computer Science</h2>
+        <p className="text-sm md:text-sm">web developer</p>
+        <p className="text-sm md:text-sm">React.js</p>
       </div>
     </div>
   );
